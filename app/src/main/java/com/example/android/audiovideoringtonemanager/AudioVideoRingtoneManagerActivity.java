bcc25d3a3342d1e166c2b1e3caa1f28e8f03f3d0
veenaw <vwagle2@gmail.com>
2016-11-05 22:19:09 +0530

    intent

--- app/src/main/java/com/example/android/audiovideoringtonemanager/AudioVideoRingtoneManagerActivity.java
+++ app/src/main/java/com/example/android/audiovideoringtonemanager/AudioVideoRingtoneManagerActivity.java
@@ -10,76 +10,75 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class AudioVideoRingtoneManagerActivity extends Activity {
-	private Ringtone mCurrentRingtone;
+    private Ringtone mCurrentRingtone;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-		// Get the default Phone Ringer RingTone and play it.
-		final Button ringtoneButton = (Button) findViewById(R.id.button1);
-		ringtoneButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				
-				Uri ringtoneUri = RingtoneManager
-						.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-				
-				playRingtone(RingtoneManager.getRingtone(
-						getApplicationContext(), ringtoneUri));
-			}
-		});
+        // Get the default Phone Ringer RingTone and play it.
+        final Button ringtoneButton = (Button) findViewById(R.id.button1);
+        ringtoneButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-		// Get the default Notification RingTone and play it.
-		final Button notifButton = (Button) findViewById(R.id.button2);
-		notifButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
+                Uri ringtoneUri = RingtoneManager
+                        .getDefaultUri(RingtoneManager.TYPE_RINGTONE);
 
-				Uri notificationUri = RingtoneManager
-						.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-				
-				playRingtone(RingtoneManager.getRingtone(
-						getApplicationContext(), notificationUri));
-			}
-		});
+                playRingtone(RingtoneManager.getRingtone(
+                        getApplicationContext(), ringtoneUri));
+            }
+        });
+
+        // Get the default Notification RingTone and play it.
+        final Button notifButton = (Button) findViewById(R.id.button2);
+        notifButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri notificationUri = RingtoneManager
+                        .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+                playRingtone(RingtoneManager.getRingtone(
+                        getApplicationContext(), notificationUri));
+            }
+        });
 
 
-		// Get the default Alarm RingTone and play it.
-		final Button alarmButton = (Button) findViewById(R.id.button3);
-		alarmButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
+        // Get the default Alarm RingTone and play it.
+        final Button alarmButton = (Button) findViewById(R.id.button3);
+        alarmButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-				Uri alarmUri = RingtoneManager
-						.getDefaultUri(RingtoneManager.TYPE_ALARM);
-				
-				playRingtone(RingtoneManager.getRingtone(
-						getApplicationContext(), alarmUri));
-			}
-		});
-	}
+                Uri alarmUri = RingtoneManager
+                        .getDefaultUri(RingtoneManager.TYPE_ALARM);
 
-	// Shut off current Ringtone and play new one
-	private void playRingtone(Ringtone newRingtone) {
+                playRingtone(RingtoneManager.getRingtone(
+                        getApplicationContext(), alarmUri));
+            }
+        });
+    }
 
-		if (null != mCurrentRingtone && mCurrentRingtone.isPlaying())
-			mCurrentRingtone.stop();
+    // Shut off current Ringtone and play new one
+    private void playRingtone(Ringtone newRingtone) {
 
-		mCurrentRingtone = newRingtone;
+        if (null != mCurrentRingtone && mCurrentRingtone.isPlaying())
+            mCurrentRingtone.stop();
 
-		if (null != newRingtone) {
-			mCurrentRingtone.play();
-		}
-	}
-	
-	@Override
-	protected void onPause() {
-		playRingtone(null);
-		super.onPause();
-	}
+        mCurrentRingtone = newRingtone;
 
+        if (null != newRingtone) {
+            mCurrentRingtone.play();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        playRingtone(null);
+        super.onPause();
+    }
 
 
 }
